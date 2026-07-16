dfdf4ff150e8d63b02922987796f3f458fc5d3a1
NgohBuilds <DeepStacker07@gmail.com>
2026-07-16 14:41:24 +0100

    FEATURE : Handle Theme Change

--- Todo_App/src/components/TodoHead.jsx
+++ Todo_App/src/components/TodoHead.jsx
@@ -1,11 +1,23 @@
 import { icons } from "../data/data.js"
 import './todoHead.css'
+import { useState } from "react"
+
 export function TodoHead()
 {
+    const [isDarkTheme, setisDarkTheme] = useState(false)
+    const html = document.documentElement
+    function handleChangeTheme(){
+        setisDarkTheme(prev => !prev)
+        html.classList.toggle('dark')
+    }
+
     return (
         <div className="header">
             <h1 id="title"> TODO</h1>
-            <img src={icons.moon} alt={icons.moon} width="26" height="26"/>
+            <img src={isDarkTheme ? icons.sun : icons.moon}
+             alt={isDarkTheme ? 'sun' : 'moon' } 
+             onClick={handleChangeTheme}
+            width="26" height="26"/>
         </div>
     )
 }
