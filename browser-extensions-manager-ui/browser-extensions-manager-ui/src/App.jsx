51075fc3d248672ed879812bc09de302d14c0f0d
NgohBuilds <DeepStacker07@gmail.com>
2026-07-30 10:30:06 +0100

    Fix : change the theme properly after clicking sun/moon Icon

--- browser-extensions-manager-ui/browser-extensions-manager-ui/src/App.jsx
+++ browser-extensions-manager-ui/browser-extensions-manager-ui/src/App.jsx
@@ -5,13 +5,22 @@ import CardExtension from './components/CardExtension'
 import { data } from './data'
 import './App.css'
 
-
+const html = document.querySelector('html')
 function App() {
+
+  const [isDark, setIsDark] = useState(false)
+
+  function handleDarkTheme(){
+
+      html.classList.contains('dark') ? setIsDark(false) : setIsDark(true)
+      html.classList.toggle('dark')    
+
+  }
 
   return (
     <>
       <section>
-        <Header/>
+        <Header onSmash={handleDarkTheme} theme={isDark}/>
         <TitleFilter title={"Extension List"}/>
         {
           data.map((cardContent, index)=>(
