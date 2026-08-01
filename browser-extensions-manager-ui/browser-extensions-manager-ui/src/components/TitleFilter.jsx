42334325e753166ea2b85f73137ebd375e8e75f6
NgohBuilds <DeepStacker07@gmail.com>
2026-08-01 04:34:54 +0100

    Refacto : Add onSmash prop to filter button

--- browser-extensions-manager-ui/browser-extensions-manager-ui/src/components/TitleFilter.jsx
+++ browser-extensions-manager-ui/browser-extensions-manager-ui/src/components/TitleFilter.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react"
 
-export function TitleFilter({title , }){
+export function TitleFilter({title , onSmash}){
+
+
+
     return (
         <div>
             <h1>{title}</h1>
             {["All", "Active", "Inactive"].map((filter, index)=> (
-                <button key={index}>{filter}</button>
+                <button key={index} onClick={onSmash}>{filter}</button>
             ))}
 
         </div>
