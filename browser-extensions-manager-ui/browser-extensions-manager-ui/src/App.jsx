c3b1d97eb944fec3ed44c531b0add86b2be68ef0
NgohBuilds <DeepStacker07@gmail.com>
2026-08-01 03:11:08 +0100

    Refacto : Add states to modify the list of extension.

--- browser-extensions-manager-ui/browser-extensions-manager-ui/src/App.jsx
+++ browser-extensions-manager-ui/browser-extensions-manager-ui/src/App.jsx
@@ -6,10 +6,11 @@ import { data } from './data'
 import './App.css'
 
 const html = document.querySelector('html')
+
 function App() {
 
   const [isDark, setIsDark] = useState(false)
-  // const [data, setData] = useState(data)
+  const [extension, setExtension] = useState(data)
 
   function handleDarkTheme(){
 
@@ -18,9 +19,26 @@ function App() {
 
   }
 
-  // function handleToggle(){
+//   function handleToggle(e, index){
 
-  // }
+//     return
+
+// }
+
+
+  function handleToggle(ind){
+
+    setExtension(previousExtension => previousExtension.map(
+      (ext, index)=> ind === index ? {...ext , isActive: !ext.isActive} : ext
+
+    ))
+
+  }
+
+  function handleDel(ind){
+    setExtension(previousExtension => previousExtension.filter(
+      (_ , index) => index != ind))
+  }
 
   return (
     <>
@@ -28,13 +46,14 @@ function App() {
         <Header onSmash={handleDarkTheme} theme={isDark}/>
         <TitleFilter title={"Extension List"}/>
         {
-          data.map((cardContent, index)=>(
+          extension.map((cardContent, index)=>(
             <CardExtension
              imgSrc={cardContent.logo}
              title={cardContent.name}
              text={cardContent.description}
              status={cardContent.isActive}
-             //handleToggle={handleToggle}
+             handleToggle={()=> {handleToggle(index)}}
+             onDelete= {()=> {handleDel(index)}}
             />
           ))
         }
